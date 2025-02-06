593601dd549737ba0eba557c34db4c79f05f05fe
Guillermo-Mora <guimormor@alu.edu.gva.es>
2025-02-06 21:05:49 +0100

    Funcionamiento inicial de los menus, cambio entre secciones y responsive completado. Sufre de bugs al cambair la resolucion de la ventana mientras ciertas clases estan activadas. Habria que refactorizarlo y gestionar mucha mas parte desde csso reiniciando valores a ciertas resoluciones

--- js/global/main.js
+++ js/global/main.js
@@ -11,15 +11,28 @@ window.addEventListener('scroll', () => {
 
 //Menu lateral desplegable
 
+const bodyMain = document.getElementById('body-main');
 const menuAside = document.querySelector('aside.menu');
 const botonMenu = document.querySelector('.header-menu-button');
 const displayContent = document.getElementById('display-content');
+const sectionBlocker = document.getElementById('aside-menu-closed');
 
 botonMenu.addEventListener('click', () => {
     menuAside.classList.toggle('menu-visible');
+    sectionBlocker.classList.toggle('active-aside-menu');
     displayContent.classList.toggle('left-spacing');
+    if (window.innerWidth <= 1440) {
+        bodyMain.classList.toggle('scroll-blocked');
+    }
 });
 
+
+if (window.innerWidth <= 1440) {
+    menuAside.classList.remove('menu-visible');
+    sectionBlocker.classList.remove('active-aside-menu');
+    displayContent.classList.remove('left-spacing');
+    bodyMain.classList.remove('scroll-blocked');
+}
 
 //Gestion de las secciones visibles en la zona principal
 
@@ -35,43 +48,49 @@ const buttonLibrary = document.getElementById('button-library');
 
 // Añade el event listener al botón "Home"
 buttonHome.addEventListener('click', () => {
-    // Verifica si ya tiene la clase .section-active
-    if (!sectionMenuHome.classList.contains('section-active')) {
-        // Quita la clase .section-active de los demás elementos
-        removeActiveClass([sectionMenuExplore, sectionMenuLibrary]);
-        removeActiveButton([buttonExplore, buttonLibrary]);
+    // Quita la clase .section-active de los demás elementos
+    removeActiveClass([sectionMenuExplore, sectionMenuLibrary]);
+    removeActiveButton([buttonExplore, buttonLibrary]);
 
-        // Añade la clase .section-active al elemento clickeado
-        sectionMenuHome.classList.add('section-active');
-        buttonHome.classList.add('button-active');
+    // Añade la clase .section-active al elemento clickeado
+    sectionMenuHome.classList.add('section-active');
+    buttonHome.classList.add('button-active');
+    if (window.innerWidth <= 1440) {
+        sectionBlocker.classList.toggle('active-aside-menu');
+        menuAside.classList.toggle('menu-visible');
+        bodyMain.classList.toggle('scroll-blocked');
     }
 });
 
 // Añade el event listener al botón "Explore"
 buttonExplore.addEventListener('click', () => {
-    // Verifica si ya tiene la clase .section-active
-    if (!sectionMenuExplore.classList.contains('section-active')) {
-        // Quita la clase .section-active de los demás elementos
-        removeActiveClass([sectionMenuHome, sectionMenuLibrary]);
-        removeActiveButton([buttonHome, buttonLibrary]);
+    // Quita la clase .section-active de los demás elementos
+    removeActiveClass([sectionMenuHome, sectionMenuLibrary]);
+    removeActiveButton([buttonHome, buttonLibrary]);
 
-        // Añade la clase .section-active al elemento clickeado
-        sectionMenuExplore.classList.add('section-active');
-        buttonExplore.classList.add('button-active');
+    // Añade la clase .section-active al elemento clickeado
+    sectionMenuExplore.classList.add('section-active');
+    buttonExplore.classList.add('button-active');
+    if (window.innerWidth <= 1440) {
+        sectionBlocker.classList.toggle('active-aside-menu');
+        menuAside.classList.toggle('menu-visible');
+        bodyMain.classList.toggle('scroll-blocked');
     }
 });
 
 // Añade el event listener al botón "Library"
 buttonLibrary.addEventListener('click', () => {
-    // Verifica si ya tiene la clase .section-active
-    if (!sectionMenuLibrary.classList.contains('section-active')) {
-        // Quita la clase .section-active de los demás elementos
-        removeActiveClass([sectionMenuHome, sectionMenuExplore]);
-        removeActiveButton([buttonHome, buttonExplore]);
+    // Quita la clase .section-active de los demás elementos
+    removeActiveClass([sectionMenuHome, sectionMenuExplore]);
+    removeActiveButton([buttonHome, buttonExplore]);
 
-        // Añade la clase .section-active al elemento clickeado
-        sectionMenuLibrary.classList.add('section-active');
-        buttonLibrary.classList.add('button-active');
+    // Añade la clase .section-active al elemento clickeado
+    sectionMenuLibrary.classList.add('section-active');
+    buttonLibrary.classList.add('button-active');
+    if (window.innerWidth <= 1440) {
+        sectionBlocker.classList.toggle('active-aside-menu');
+        menuAside.classList.toggle('menu-visible');
+        bodyMain.classList.toggle('scroll-blocked');
     }
 });
 
